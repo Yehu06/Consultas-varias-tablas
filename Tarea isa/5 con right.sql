ea36db78548244980077bcfb992dd1b5b1f012a9
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2025-04-02 09:39:35 +0200

    Update 5 con right.sql

--- Tarea isa/5 con right.sql
+++ Tarea isa/5 con right.sql
@@ -2,7 +2,7 @@
 
 --Hace lo mismo que el left pero este comienza por la tabla lugar en vez de jesuita
 
-SELECT jesuita.nombre AS Jesuita, lugar.lugar
+SELECT jesuita.nombre, lugar.lugar
 FROM lugar
 RIGHT JOIN visita ON lugar.ip = visita.ip
 RIGHT JOIN jesuita ON visita.idJesuita = jesuita.idJesuita;
