36b94c04144a8b5148a709eead6aefe980587893
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2025-04-02 09:38:41 +0200

    Update 1.sql

--- Tarea isa/1.sql
+++ Tarea isa/1.sql
@@ -1,5 +1,5 @@
 --1--
-SELECT visita.idVisita, jesuita.nombre AS Jesuita, visita.fechaHora
+SELECT visita.idVisita, jesuita.nombre, visita.fechaHora
 FROM visita
-INNER JOIN jesuita ON visita.idJesuita = jesuita.idJesuita;--relaciona visita con jesuita a través del campo idJesuita.
+INNER JOIN jesuita ON visita.idJesuita = jesuita.idJesuita;--relaciona visita con jesuita a travÃ©s del campo idJesuita.
 
