04f98b9980552c6a89d4f1e691de5c8a9bd1c866
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2025-04-02 09:39:07 +0200

    Update 2.sql

--- Tarea isa/2.sql
+++ Tarea isa/2.sql
@@ -1,5 +1,5 @@
 --2--
-SELECT visita.idVisita, jesuita.nombre AS Jesuita, lugar.lugar, visita.fechaHora
+SELECT visita.idVisita, jesuita.nombre, lugar.lugar, visita.fechaHora
 FROM visita
 INNER JOIN jesuita ON visita.idJesuita = jesuita.idJesuita-- une visita con jesuita mediante idJesuita.
 INNER JOIN lugar ON visita.ip = lugar.ip; -- une visita con lugar mediante ip.
