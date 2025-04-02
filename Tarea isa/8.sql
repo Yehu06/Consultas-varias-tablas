78cdb2def93d06feb7b036e53f0b287f861e3b03
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2025-04-02 09:40:06 +0200

    Update 8.sql

--- Tarea isa/8.sql
+++ Tarea isa/8.sql
@@ -1,12 +1,12 @@
 --8--
 --Utilzamos el left join paar visualizar todos los jesuitas y sus lugares visitados
-SELECT jesuita.nombre AS Jesuita, lugar.lugar
+SELECT jesuita.nombre, lugar.lugar
 FROM jesuita
 LEFT JOIN visita ON jesuita.idJesuita = visita.idJesuita
 LEFT JOIN lugar ON visita.ip = lugar.ip;
 
 --Ahora utilizamos el right para ver los jesuitas y los lugares incluyendo los que no se han visitado
-SELECT jesuita.nombre AS Jesuita, lugar.lugar
+SELECT jesuita.nombre, lugar.lugar
 FROM lugar
 RIGHT JOIN visita ON lugar.ip = visita.ip
 RIGHT JOIN jesuita ON visita.idJesuita = jesuita.idJesuita;
